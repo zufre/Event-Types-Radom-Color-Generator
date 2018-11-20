51e6d983191e4c4c4f84d7e217f2776c815ab842
Cédric David <cedric.david@btopenworld.com>
2018-11-20 12:56:08 +1000

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+// This variable stores the "Pick a Color" button
+let button = document.getElementById('color-button');
+
+// This variable stores the "Mystery Color" button
+let mysteryButton = document.getElementById('next-button');
+
+// This random number function that will creates color codes for the randomColor variable
+function rgb(num) {
+  return Math.floor(Math.random() * num);
+}
+
+// Write your code below
+let colorChange = function() {
+  let randomColor = 'rgb(' + rgb(255) + ',' + rgb(255) + ',' + rgb(255) + ')';
+  event.target.style.backgroundColor = randomColor;
+}
+button.onclick = colorChange;
+mysteryButton.onwheel = colorChange;
